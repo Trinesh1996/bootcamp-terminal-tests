86792d2e77ec85d73d7cf25d5cac0779c397b24e
Trinesh Chetty <trineshchetty@gmail.com>
2018-07-25 18:36:55 +0200

    update package.json,travis.yml

--- tests/isWeekdayTest.js
+++ tests/isWeekdayTest.js
@@ -2,6 +2,7 @@ let assert = require("assert");
 let isWeekday = require("../isWeekday.js")
 
 
+
 describe("isWeekday", function(){
   it('should return a specific day of the week', function(){
     assert.equal(isWeekday('Monday'), true);
@@ -11,3 +12,4 @@ describe("isWeekday", function(){
       assert.equal(isWeekday("Saturday"), false);
     });
 });
+
